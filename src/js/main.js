4bc0b29a3c55f57fa1ad2338ce1b1e42ccd4b2ef
Евгений <57830696+Salabaykin@users.noreply.github.com>
2020-05-23 15:31:50 +0300

    add new sections

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,11 @@ $(document).ready(function(){
     arrows: false,
     fade: true,
     cssEase: 'linear',
-    asNavFor: '.progress-slider__dots'
+    asNavFor: '.progress-slider__dots',
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    pauseOnHover: false
   });
 
   $('.progress-slider__dots').slick({
